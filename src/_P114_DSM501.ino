9bae288545373049cc685c48e6543e8ffcdd5a59
fedor <fedor@yu.wildpark.net>
2019-04-12 11:27:02 +0300

    Update _P114_DSM501.ino

--- src/_P114_DSM501.ino
+++ src/_P114_DSM501.ino
@@ -12,34 +12,58 @@
 #define DELTA_MIN 10000  //10ms
 #define DELTA_MAX 90000  //90ms
 
-unsigned long Plugin_114_pulseCounter[TASKS_MAX];
-unsigned long Plugin_114_pulseTotalCounter[TASKS_MAX];
-unsigned long Plugin_114_pulseTime[TASKS_MAX];
-unsigned long Plugin_114_pulseTimePrevious[TASKS_MAX];
-unsigned long tstart1, tstart2;
-unsigned long tduration = 30000; // duration of measurement in ms
-//unsigned long triggerOn; // start of pulse time in us
-//unsigned long triggerOff; // end of pulse time in us
-//unsigned long lowpulseoccupancy; // duration of pulse in us
-volatile unsigned long thigh1, thigh2;
-volatile unsigned long tlow1, tlow2;
-volatile unsigned long startlow1, startlow2;
-volatile unsigned long starthigh1, starthigh2;
-volatile boolean done1, done2;
-volatile boolean value1, value2;
-//boolean trigger = false;
-volatile float ratio1, ratio2;
-volatile int DMSpin1, DMSpin2;
-float dens1, dens2;
-
 typedef struct{
-	volatile unsigned long start;
-	volatile unsigned int cnt;
-	volatile unsigned long low;
-  volatile unsigned long high;
+	unsigned long start;
+	int cnt;
+	long low;
+  long high;
+  int drop;
 } pm_114;
 
-pm_114 data_114[2];
+pm_114 Plugin_114_data[2];
+char flag=0;
+
+void Plugin_114_var(char d){
+		Plugin_114_data[d].start=micros();
+		Plugin_114_data[d].cnt=0;
+}
+
+double Plugin_114_val(char d) {
+	 //* with data sheet...regression function is
+	 //* 	y=0.30473*x^3-2.63943*x^2+102.60291*x-3.49616
+   //ratio = lowpulseoccupancy / (sampletime_ms*10.0);
+	double r = Plugin_114_data[d].low /Plugin_114_data[d].high;
+	double weight = 0.30473 * pow(r, 3) - 2.63943 * pow(r, 2) + 102.60291 * r - 3.49616;
+  Plugin_114_data[d].low=Plugin_114_data[d].high=0;
+	return weight < 0.0 ? 0.0 : weight;
+}
+
+int Plugin_114_AQI(float pm1, float pm2) {
+	int aqi = -1;
+
+	double P25Weight = pm1 - pm2;
+	if (P25Weight>= 0 && P25Weight <= 15.4) {
+		aqi = 0   +(int)(50.0 / 15.5 * P25Weight);
+	} else if (P25Weight > 15.5 && P25Weight <= 40.5) {
+		aqi = 50  + (int)(50.0 / 25.0 * (P25Weight - 15.5));
+	} else if (P25Weight > 40.5 && P25Weight <= 65.5) {
+		aqi = 100 + (int)(50.0 / 25.0 * (P25Weight - 40.5));
+	} else if (P25Weight > 65.5 && P25Weight <= 150.5) {
+		aqi = 150 + (int)(50.0 / 85.0 * (P25Weight - 65.5));
+	} else if (P25Weight > 150.5 && P25Weight <= 250.5) {
+		aqi = 200 + (int)(100.0 / 100.0 * (P25Weight - 150.5));
+	} else if (P25Weight > 250.5 && P25Weight <= 350.5) {
+		aqi = 300 + (int)(100.0 / 100.0 * (P25Weight - 250.5));
+	} else if (P25Weight > 350.5 && P25Weight <= 500.0) {
+		aqi = 400 + (int)(100.0 / 150.0 * (P25Weight - 350.5));
+	} else if (P25Weight > 500.0) {
+		aqi = 500 + (int)(500.0 / 500.0 * (P25Weight - 500.0)); // Extension
+	} else {
+		aqi = -1; // Initializing
+	}
+
+	return aqi;
+}
 
 boolean Plugin_114(byte function, struct EventStruct *event, String& string)
 {
@@ -78,75 +102,67 @@ boolean Plugin_114(byte function, struct EventStruct *event, String& string)
 
     case PLUGIN_WEBFORM_LOAD:
       {
-        char tmpString[128];
+/*        char tmpString[128];
         sprintf_P(tmpString, PSTR("<TR><TD>Averaging Time (mSec):<TD><input type='text' name='plugin_114' value='%u'>"), Settings.TaskDevicePluginConfig[event->TaskIndex][0]);
         string += tmpString;
-        success = true;
+*/        success = true;
         break;
       }
 
     case PLUGIN_WEBFORM_SAVE:
       {
-        String plugin1 = WebServer.arg("plugin_114");
+/*        String plugin1 = WebServer.arg("plugin_114");
         Settings.TaskDevicePluginConfig[event->TaskIndex][0] = plugin1.toInt();
         // tduration= Settings.TaskDevicePluginConfig[event->TaskIndex][0];
-        success = true;
+*/        success = true;
         break;
       }
-
 
     case PLUGIN_INIT:
       {
         String log = F("INIT : DSM501A ");
         log += Settings.TaskDevicePin1[event->TaskIndex];
-        addLog(LOG_LEVEL_INFO, log);
-        // tduration= Settings.TaskDevicePluginConfig[event->TaskIndex][0];
-//        tstart1 = millis();
-//        startlow1 = micros();
-//        starthigh1 = startlow1;
-        DMSpin1 = Settings.TaskDevicePin1[event->TaskIndex];
+
+        Plugin_114_data[0].drop=0;
+        Plugin_114_data[1].drop=0;
+        
+        int DMSpin1 = Settings.TaskDevicePin1[event->TaskIndex];
         pinMode(DMSpin1, INPUT);
-				data_114[0].start=micros();
-				data_114[0].cnt=0;
+       	Plugin_114_var(0);
         attachInterrupt(digitalPinToInterrupt(DMSpin1), Plugin_114_ISR1, CHANGE);
-//        tstart2 = millis();
-//        startlow2 = micros();
-//        starthigh2 = startlow2;
-        DMSpin2 = Settings.TaskDevicePin2[event->TaskIndex];
+
+        int DMSpin2 = Settings.TaskDevicePin2[event->TaskIndex];
         pinMode(DMSpin2, INPUT);
-				data_114[1].start=micros();
-				data_114[0].cnt=0;
+        Plugin_114_var(1);
         attachInterrupt(digitalPinToInterrupt(DMSpin2), Plugin_114_ISR2, CHANGE);
+
+        log += F("OK");
+        addLog(LOG_LEVEL_INFO, log);
         success = true;
         break;
       }
 
     case PLUGIN_READ:
 		{
+      flag++;
       String log = F("DSM501A: PM1.0=");
-			if(data_144[0].cnt>0){
-			}
-      UserVar[event->BaseVarIndex] = (float) dens1;
+      UserVar[event->BaseVarIndex] = (float) Plugin_114_val(0);
+      log += UserVar[event->BaseVarIndex];
       log += F(" PM2.5=");
-      log += dens2;
+      UserVar[event->BaseVarIndex + 1] = (float) Plugin_114_val(1);
+      log += UserVar[event->BaseVarIndex+1];
+      log += F(" AQI=");
+      log += Plugin_114_AQI(UserVar[event->BaseVarIndex],UserVar[event->BaseVarIndex+1]);
+      log += F(" Drop=");
+      log += Plugin_114_data[0].drop;
+      log += F("/");
+      log += Plugin_114_data[1].drop;
       addLog(LOG_LEVEL_INFO, log);
-      UserVar[event->BaseVarIndex + 1] = (float) dens2;
+      Plugin_114_data[0].drop=0;
+      Plugin_114_data[1].drop=0;
 
-/*        if (done1 && done2) {
-          done1 = FALSE;
-          dens1 = ratio1 * 110; // ug/m^3
-          done2 = FALSE;
-          dens2 = ratio2 * 110; // ug/m^3
-
-          String log = F("DSM501A: PM1.0=");
-          log += dens1;
-          log += F(" PM2.5=");
-          log += dens2;
-          addLog(LOG_LEVEL_INFO, log);
-          UserVar[event->BaseVarIndex] = (float) dens1;
-          UserVar[event->BaseVarIndex + 1] = (float) dens2;
-        }
-*/        success = true;
+      success = true;
+      flag=0;
         break;
       }
   }
@@ -154,61 +170,48 @@ boolean Plugin_114(byte function, struct EventStruct *event, String& string)
 }
 
 
-/*********************************************************************************************\
+/*
   Check Pulse (called from irq handler)
-  \*********************************************************************************************/
+*/
+void Plugin_114_add(char d){
+  if(flag)return;
+	 int delta=micros()-Plugin_114_data[d].start;
+   int pin= d?Settings.TaskDevicePin1[event->TaskIndex]:Settings.TaskDevicePin2[event->TaskIndex];
+   if(digitalRead(pin) ==0){//read input pin just changed
+      if(Plugin_114_data[d].cnt){
+        Plugin_114_data[d].high+=delta;
+      }
+      Plugin_114_var(d);
+	 }else{
+			if(delta>DELTA_MIN && delta<DELTA_MAX){
+				Plugin_114_data[d].cnt++;
+				Plugin_114_data[d].low+=delta;
+			}else{
+        Plugin_114_data[d].drop++;        
+      }
+	 }
+}
+
 void Plugin_114_ISR1()
 {
-   if(digitalRead(DMSpin1) ==0){//read input pin just changed
-			data_114[0].start=micros();
-	 }else{
-			data_114[0].start=micros();
-			int delta=micros()-data_114[0].start;
-			if(delta>DELTA_MIN && delta<DELTA_MAX){
-				data_114[0].cnt++;
-				data_114[0].low+=delta;
-			}
-	 }
-/*  if (value1 == 0) { // gone low
-    startlow1 = micros(); // record starting of low period
-    thigh1 += startlow1 - starthigh1; // record duration of past high state
-  } else { // gone high
-    starthigh1 = micros(); // record starting of high period
-    tlow1 += starthigh1 - startlow1; // record duration of past low state
-  }
-  if (millis() > tstart1 + tduration) { // check if average time has past
-    tstart1 = millis(); // reset time period
-    ratio1 = float(tlow1) / float(thigh1 + tlow1) * 100; // compute ratio low to total
-    tlow1 = 0; // reset low time counter
-    thigh1 = 0; // reset high time counter
-    done1 = TRUE; // set reading complete flag
-  }
- */
+  Plugin_114_add(0);
 }
 /*********************************************************************************************\
   Check Pulse (called from irq handler)
   \*********************************************************************************************/
 void Plugin_114_ISR2()
 {
-  value2 = digitalRead(DMSpin2); //read input pin just changed
-  if (value2 == 0) { // gone low
-    startlow2 = micros(); // record starting of low period
-    thigh2 += startlow2 - starthigh2; // record duration of past high state
-  } else { // gone high
-    starthigh2 = micros(); // record starting of high period
-    tlow2 += starthigh2 - startlow2; // record duration of past low state
-  }
-  if (millis() > tstart2 + tduration) { // check if average time has past
-    tstart2 = millis(); // reset time period
-    ratio2 = float(tlow2) / float(thigh2 + tlow2) * 100; // compute ratio low to high
-    tlow2 = 0; // reset low time counter
-    thigh2 = 0; // reset high time counter
-    done2 = TRUE; // set reading complete flag
-  }
+  Plugin_114_add(1);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
- /*#include "DSM501.h"
+ /*
+        high
+ __|     _____ |     __
+   |____|     |____|
+   |low |
+ 
+ #include "DSM501.h"
 #include <avr/interrupt.h>
 
 
